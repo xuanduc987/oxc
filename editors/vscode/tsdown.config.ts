9ba589d53a88d81d4c157c3a51dec88e743e4d32
Kevin Deng <sxzz@sxzz.moe>
2025-10-30 09:53:13 +0800

    fix: bundle vscode-languageclient

--- editors/vscode/tsdown.config.ts
+++ editors/vscode/tsdown.config.ts
@@ -3,10 +3,10 @@ import process from 'node:process';
 
 export default defineConfig({
   entry: process.env.TEST === 'true' ? 'tests/**/*.ts' : { main: 'client/extension.ts' },
+  outDir: 'out',
   external: ['vscode'],
   platform: 'node',
   target: 'node20',
   sourcemap: true,
   minify: true,
-  outDir: 'out',
 });
